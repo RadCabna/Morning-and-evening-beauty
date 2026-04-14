d173fa350aeec4c237077e9826fbe879dcdecf94
Radcabna <radcabna@gmail.com>
2026-04-14 12:01:19 +0300

    Initial Commit

---
+++ Morning_beauty/Morning_beautyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Morning_beautyApp.swift
+//  Morning_beauty
+//
+//  Created by Алкександр Степанов on 14.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Morning_beautyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
